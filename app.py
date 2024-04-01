9c0c4246efd327da9085756cca0a55c812f6db13
ogatakatsuya <katsuya151225@icloud.com>
2024-04-01 17:33:15 +0900

    update

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask,render_template,request,url_for,redirect
 from forms import City
 import requests
+from pprint import pprint
+import json
+from datetime import datetime, timedelta, timezone
 
 app = Flask(__name__)
 
@@ -11,28 +14,61 @@ app.config['SECRET_KEY'] = os.urandom(24)
 def search():
     form = City()
     if request.method == "POST":
+        
         city = form.city.data
         API_KEY = '7737025f668c0d4718e526c348467778' # 自分のAPIキーに置き換えてください。 
-
-        BASE_URL = 'http://api.openweathermap.org/data/2.5/weather'
+        BASE_URL = 'https://api.openweathermap.org/data/2.5/forecast'
         PARAMS = {
             'q': f'{city},JP',  # 東京都
             'units': 'metric',  # 温度を摂氏で取得
-            'appid': API_KEY
+            'appid': API_KEY,
+            'lang': 'ja'
         }
         response = requests.get(BASE_URL, params=PARAMS)
-        data = response.json()
+        datas = response.json()
+        tz = timezone(timedelta(hours=+9),"JST")
+        
+        city = datas['city']['name']
+        
+        all_data_list = []
 
-        if response.status_code == 200:
-            temp = data["main"]["temp"]
-            humidity = data["main"]["humidity"]
-            wind = data["wind"]["speed"]
-            weather = data["weather"][0]["main"]
-            name = data["name"]
-            return render_template('result.html',temp=temp,humidity=humidity,wind=wind,weather=weather,name=name)
-        else:
-            print(f"エラー: {data['message']}")
-    return render_template('search.html',form=form)
+        for index,data in enumerate(datas['list']):
+            
+            if(index == 0):
+                date = data['dt_txt']
+                temp = data['main']['temp']
+                humidity = data['main']['humidity']
+                description = data['weather'][0]['description']
+                icon = 'images/' + data['weather'][0]['icon']
+                
+                first_data = {
+                    'date': date,
+                    'temp': temp,
+                    'humidity': humidity,
+                    'description': description,
+                    'icon': icon
+                }
+                
+                continue
+            
+            date = data['dt_txt']
+            temp = data['main']['temp']
+            humidity = data['main']['humidity']
+            description = data['weather'][0]['description']
+            icon = 'images/' + data['weather'][0]['icon']
+            
+            single_data = {
+                'date': date,
+                'temp': temp,
+                'humidity': humidity,
+                'description': description,
+                'icon': icon
+            }
+            
+            all_data_list.append(single_data)
+        return render_template('result.html',city = city, list = all_data_list, first_data = first_data)
+    
+    return render_template('search.html',form = form)
 
 if __name__ == "__main__":
     app.run(debug=True)
