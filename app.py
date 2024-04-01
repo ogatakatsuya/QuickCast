0ad24968a9c511276d576b9227815d71bb646188
ogatakatsuya <katsuya151225@icloud.com>
2024-04-01 21:46:59 +0900

    日付をJSTに変更

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from forms import City
 import requests
 from pprint import pprint
 import json
-from datetime import datetime, timedelta, timezone
+import datetime
 
 app = Flask(__name__, static_folder='./templates/images')
 
@@ -16,26 +16,32 @@ def search():
     if request.method == "POST":
         
         city = form.city.data
-        API_KEY = '7737025f668c0d4718e526c348467778' # 自分のAPIキーに置き換えてください。 
+        API_KEY = '7737025f668c0d4718e526c348467778'
         BASE_URL = 'https://api.openweathermap.org/data/2.5/forecast'
         PARAMS = {
-            'q': f'{city},JP',  # 東京都
-            'units': 'metric',  # 温度を摂氏で取得
+            'q': f'{city},JP',
+            'units': 'metric',
             'appid': API_KEY,
             'lang': 'ja'
         }
         response = requests.get(BASE_URL, params=PARAMS)
         datas = response.json()
-        tz = timezone(timedelta(hours=+9),"JST")
         
         city = datas['city']['name']
-        
         all_data_list = []
 
         for index,data in enumerate(datas['list']):
             
             if(index == 0):
-                date = data['dt_txt']
+                utc_timestamp = data['dt']
+                jst_timestamp = utc_timestamp + 9*3600
+
+                jst_dt = datetime.datetime.utcfromtimestamp(jst_timestamp)
+                month = str(jst_dt.month).zfill(2)
+                day = str(jst_dt.day).zfill(2)
+                hour = str(jst_dt.hour).zfill(2)
+                minute = str(jst_dt.minute).zfill(2)
+                date = f"{month}/{day} {hour}:{minute}"
                 temp = round(data['main']['temp'])
                 humidity = data['main']['humidity']
                 description = data['weather'][0]['description']
@@ -51,7 +57,15 @@ def search():
                 
                 continue
             
-            date = data['dt_txt']
+            utc_timestamp = data['dt']
+            jst_timestamp = utc_timestamp + 9*3600
+
+            jst_dt = datetime.datetime.utcfromtimestamp(jst_timestamp)
+            month = str(jst_dt.month).zfill(2)
+            day = str(jst_dt.day).zfill(2)
+            hour = str(jst_dt.hour).zfill(2)
+            minute = str(jst_dt.minute).zfill(2)
+            date = f"{month}/{day} {hour}:{minute}"
             temp = round(data['main']['temp'])
             humidity = data['main']['humidity']
             description = data['weather'][0]['description']
@@ -69,6 +83,7 @@ def search():
         return render_template('result.html',city = city, list = all_data_list, first_data = first_data)
     
     return render_template('search.html',form = form)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
